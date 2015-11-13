de571b59b52235b15d024d34bb9c8d106f1e6905
Hubert <hubert.dworczynski@gmail.com>
2015-11-13 22:20:50 +0100

    modified to use https

--- js/event_page.js
+++ js/event_page.js
@@ -1,5 +1,5 @@
 chrome.omnibox.onInputEntered.addListener(function callback(subreddit) {
 	chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
-	  chrome.tabs.update( tabs[0].id, { url: "http://www.reddit.com/r/" + subreddit } ); 
+	  chrome.tabs.update( tabs[0].id, { url: "https://www.reddit.com/r/" + subreddit } ); 
 	});
 });
